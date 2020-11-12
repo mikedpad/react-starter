688766aa93384029ac1977a914132de1c1415c57
Michael Daniel Padilla <michaeldpadilla@gmail.com>
2020-11-12 15:45:40 -0800

    Default fonts modified

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,3 +1,3 @@
-export const serif = `Lora, 'Times New Roman', Times, serif`;
-export const sansSerif = `'Source Sans Pro', Helvetica, Arial, sans- serif`;
-export const monospace = `'Source Code Pro', Inconsolata, Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace`;
+export const serif = `'Times New Roman', Times, serif`;
+export const sansSerif = `Helvetica, Arial, sans- serif`;
+export const monospace = `Inconsolata, Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace`;
